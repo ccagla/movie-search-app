c6a611f24b9735d0e07ee6059d492a88ae096479
cagla eksi <caglaeksii@gmail.com>
2025-07-11 14:58:21 +0300

    feat: display search results as movie cards

--- script.js
+++ script.js
@@ -26,3 +26,39 @@ function showError(message) {
     }, 3000);
     
 }
+
+async function fetchMovies(query) {
+    try {
+       const res = await fetch(`https://www.omdbapi.com/?apikey=e713582&s=${query}`);
+
+        const data = await res.json();
+
+if (data.Response === "False") {
+    showError(data.Error || "No results Found.");
+    return;
+}
+
+displayMovies(data.Search);
+
+    } catch (error) {
+        showError("An error occurred while fetching data.");
+        console.log(error);
+    }
+}
+
+function displayMovies(movies) {
+        movieList.innerHTML = "";
+
+        movies.forEach(movie => {
+            const movieCard = document.createElement("div");
+            movieCard.classList.add("movie-card");
+
+            movieCard.innerHTML = `
+            <img src="${movie.Poster !== "N/A" ? movie.Poster : "https://via.placeholder.com/150"}" alt="${movie.Title}">
+            <h3>${movie.Title}</h3>
+      <p>${movie.Year}</p>
+            `;
+
+            movieList.appendChild(movieCard)
+        });
+}
